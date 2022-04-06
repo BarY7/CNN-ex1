d43bb0c49956ae746209deca019a5084febda7bf
BarY7 <baryaacovi7@gmail.com>
2022-04-06 20:22:46 +0300

    remove np abs

--- hw1/linear_regression.py
+++ hw1/linear_regression.py
@@ -162,11 +162,11 @@ def top_correlated_features(df: DataFrame, target_feature, n=5):
         cor_xx_cor_yy = ((compute_cor(df, col).sum()) ** 0.5) * \
             ((compute_cor(df, target_feature).sum()) ** 0.5)
         # print(up)
-        correlation = np.append(correlation, np.abs(cor_xy/cor_xx_cor_yy))
+        correlation = np.append(correlation, cor_xy/cor_xx_cor_yy)
     correlation_abs = np.abs(correlation) 
     max_indices = np.argpartition(correlation_abs, n * -1)[n * (-1):][::-1]
     top_n_features = columns_after_drop[max_indices]
-    top_n_corr = correlation_abs[max_indices]
+    top_n_corr = correlation[max_indices]
     # ========================
 
     return top_n_features, top_n_corr
